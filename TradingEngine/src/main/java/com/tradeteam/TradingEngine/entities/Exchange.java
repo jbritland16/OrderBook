e21e6b18b5ca154dba57b4478e2d9d807c7bee92
Julia Britland <jbritland@albanybeck.com>
2023-03-24 15:20:38 +0000

    Worked on Exchange entity

--- TradingEngine/src/main/java/com/tradeteam/TradingEngine/entities/Exchange.java
+++ TradingEngine/src/main/java/com/tradeteam/TradingEngine/entities/Exchange.java
@@ -1,4 +1,24 @@
 package com.tradeteam.TradingEngine.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter @AllArgsConstructor @NoArgsConstructor
 public class Exchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String exchangeId;
+
+    @OneToMany(mappedBy = "exchangeId", cascade = CascadeType.ALL)
+    private List<OrderBook> orderBooks;
+
+    // private double feeLadder; This was included in Kishore's example but I would like
+    // to do more research before implementing
+
 }
