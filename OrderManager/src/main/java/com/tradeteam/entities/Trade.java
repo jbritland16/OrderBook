174178100c6c50e8c62418b98448532e43b7a16e
Julia Britland <jbritland@albanybeck.com>
2023-04-05 16:28:10 +0100

    Added methods to Trade and Order to format timestamp and price and return as string, added these to HTML files for better printing

--- OrderManager/src/main/java/com/tradeteam/entities/Trade.java
+++ OrderManager/src/main/java/com/tradeteam/entities/Trade.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter @AllArgsConstructor @NoArgsConstructor @ToString
 public class Trade {
@@ -15,5 +18,15 @@ public class Trade {
     private int numberTraded;
     private double pricePerShare;
     private Order connectedOrder;
+
+    public String getFormattedTimestamp() {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd 'at' HH:mm:ss");
+        return df.format(this.tradeTimestamp);
+    }
+
+    public String getFormattedPrice(String currencySymbol) {
+        DecimalFormat df = new DecimalFormat(currencySymbol + "0.00");
+        return df.format(this.pricePerShare);
+    }
 
 }
